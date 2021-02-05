3f8964cf1cbb87da688351b30571ca1bff1cc20e
Lucas Livchits <livchitsl@gmail.com>
2021-02-05 20:50:25 -0300

    Add state and pass data to component

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,17 +1,30 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
 
 import Answers from '../Answers/Answers';
 import QuestionData from '../QuestionData/QuestionData';
 
 import style from './Question.module.scss';
 
-function Question() {
+function Question({ questionsData }) {
+  const [currentQuestion, setCurrentQuestion] = React.useState(0);
+  const { category, difficulty, question } = questionsData[currentQuestion];
+
   return (
     <main className={style.question}>
-      <QuestionData />
+      <QuestionData
+        category={category}
+        difficulty={difficulty}
+        number={currentQuestion + 1}
+        question={question}
+      />
       <Answers />
     </main>
   );
 }
+
+Question.propTypes = {
+  questionsData: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default Question;
