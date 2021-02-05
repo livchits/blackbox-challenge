a9937d0799288283076ed7325cbbfba8ed53aa3d
Lucas Livchits <livchitsl@gmail.com>
2021-02-05 20:40:39 -0300

    Add useEffect to get questions and state for the data

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 
 import Question from '../components/Question/Question';
+import getQuestions from '../api';
 
 import style from './App.module.scss';
 
 function App() {
+  const [questions, setQuestions] = React.useState();
+
+  React.useEffect(() => {
+    getQuestions().then((data) => setQuestions(data));
+  }, []);
+
+  if (questions) {
+    return (
+      <div className={style.container}>
+        <main className={style.card}>
+          <Question questionsData={questions} />
+        </main>
+      </div>
+    );
+  }
+
   return (
     <div className={style.container}>
-      <Question />
+      <main className={style.card}>Loading...</main>
     </div>
   );
 }
