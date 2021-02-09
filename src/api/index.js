9a77f243dda0fbd7da1c1ec69b20cd2492b8cf56
Lucas Livchits <livchitsl@gmail.com>
2021-02-09 19:58:10 -0300

    Change api to use then instead of async/await

--- src/api/index.js
+++ src/api/index.js
@@ -2,16 +2,15 @@ const QUESTIONS_AMOUNT = 10;
 const API_URL = `https://opentdb.com/api.php?amount=${QUESTIONS_AMOUNT}`;
 
 async function api() {
-  try {
-    const response = await fetch(API_URL);
-    if (response.ok) {
-      const { results: data } = await response.json();
-      return { data, error: !response.ok };
-    }
-    throw new Error(response.statusText);
-  } catch (error) {
-    return { error: true, message: error.message };
-  }
+  return fetch(API_URL)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(({ results: data }) => ({ data, error: false }))
+    .catch((error) => ({ error: true, message: error.message }));
 }
 
 export default api;
