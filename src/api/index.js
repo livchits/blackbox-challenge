2ea08fea135e4f6f27befc84b20dd00b68940cf2
Lucas Livchits <livchitsl@gmail.com>
2021-02-10 19:19:13 -0300

    Use parseData function

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,5 @@
+import parseData from '../utils/parseData';
+
 const QUESTIONS_AMOUNT = 10;
 const API_URL = `https://opentdb.com/api.php?amount=${QUESTIONS_AMOUNT}`;
 
@@ -5,7 +7,8 @@ async function api() {
   try {
     const response = await fetch(API_URL);
     if (response.ok) {
-      const { results: data } = await response.json();
+      const { results } = await response.json();
+      const data = results.map((question) => parseData(question));
       return { data, error: !response.ok };
     }
     throw new Error(response.statusText);
