fbe3e71a5853eacab6504730a9b4e8d8992e8fb2
Lucas Livchits <livchitsl@gmail.com>
2021-02-09 23:16:10 -0300

    Add button to restart game

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -4,13 +4,27 @@ import Card from '../components/Card/Card';
 import Question from '../components/Question/Question';
 import useQuestions from '../hooks/useQuestions';
 
+import style from './App.module.scss';
+
 function App() {
   const [isPlaying, setIsPlaying] = React.useState(true);
   const [points, setPoints] = React.useState(0);
   const { status, questions } = useQuestions(isPlaying);
 
+  const restartGame = () => {
+    setIsPlaying(true);
+    setPoints(0);
+  };
+
   if (!isPlaying) {
-    return <Card>{`You won ${points} points!`}</Card>;
+    return (
+      <Card>
+        {`You won ${points} points!`}
+        <button className={style.restart} onClick={restartGame}>
+          Play again
+        </button>
+      </Card>
+    );
   }
 
   if (status === 'pending') {
