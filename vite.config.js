9e7b6b4a7e9ace5465e021e32b8cbe587bdcc413
Lucas Livchits <livchitsl@gmail.com>
2021-04-27 12:19:59 -0300

    Add Vite Fonts plugin

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import svgr from 'vite-plugin-svgr';
+import ViteFonts from 'vite-plugin-fonts';
 
 /**
  * https://vitejs.dev/config/
  * @type { import('vite').UserConfig }
  */
 export default {
-  plugins: [reactRefresh(), svgr()],
+  plugins: [
+    reactRefresh(),
+    svgr(),
+    ViteFonts({
+      google: {
+        families: [{ name: 'Ubuntu', styles: 'wght@500;700' }],
+      },
+    }),
+  ],
 };
