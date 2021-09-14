d4aaabc1945abfcd9ff0761f7409fa00971dc1cf
Lucas Livchits <livchitsl@gmail.com>
2021-09-14 19:22:41 -0300

    Add types for Card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,18 +1,13 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
 
 import style from './Card.module.scss';
 
-function Card({ children }) {
+function Card({ children }: { children: React.ReactNode }) {
   return (
     <div className={style.container}>
       <main className={style.card}>{children}</main>;
     </div>
   );
 }
-
-Card.propTypes = {
-  children: PropTypes.node.isRequired,
-};
 
 export default Card;
