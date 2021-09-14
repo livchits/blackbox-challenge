dc9f9d83490600981225e649cdca1c352a404dcb
Lucas Livchits <livchitsl@gmail.com>
2021-09-14 19:39:37 -0300

    Add types for AnswerResult component and svg imports

--- src/components/AnswerResult/AnswerResult.tsx
+++ src/components/AnswerResult/AnswerResult.tsx
@@ -1,23 +1,23 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
 
 import { ReactComponent as Checkmark } from '../../assets/checkmark.svg';
 import { ReactComponent as IconX } from '../../assets/icon-X.svg';
 
 import style from './AnswerResult.module.scss';
 
-function AnswerResult({ isCorrect, show }) {
+interface AnswerResultProps {
+  isCorrect: boolean;
+  show: boolean;
+}
+
+function AnswerResult({ isCorrect, show }: AnswerResultProps) {
   const visibility = show ? 'show' : 'hidden';
+
   return isCorrect ? (
     <Checkmark className={style[visibility]} />
   ) : (
     <IconX className={style[visibility]} />
   );
 }
-
-AnswerResult.propTypes = {
-  isCorrect: PropTypes.bool.isRequired,
-  show: PropTypes.bool.isRequired,
-};
 
 export default AnswerResult;
